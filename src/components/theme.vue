d741f1ad545bd0a88e8735b2d7762eb598e4ba69
markthree <1801982702@qq.com>
2023-01-20 17:39:52 +0800

    refactor: add locale

--- src/components/theme.vue
+++ src/components/theme.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
-import { NConfigProvider } from 'naive-ui'
+import { NConfigProvider, zhCN, dateZhCN } from 'naive-ui'
 import { naiveTheme, SwitchIcon } from 'vue-dark-switch'
 import 'vue-dark-switch/dist/style.css'
 </script>
 
 <template>
-	<NConfigProvider :theme="naiveTheme">
+	<NConfigProvider
+		:theme="naiveTheme"
+		:locale="zhCN"
+		:date-locale="dateZhCN">
 		<div class="flex justify-end p-3">
 			<SwitchIcon />
 		</div>
